e2b057283edab6a6adc432361ab96da8de3fde88
rahul <rahul.krisx@gmail.com>
2018-09-04 01:20:35 -0700

    new

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
 from datetime import datetime
-from app import db
+from flask_login import UserMixin
+from app import login
+from app import db  
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -9,4 +15,13 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
 
